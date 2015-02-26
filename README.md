d8c117759c4d5df6a40074cd54d8787f5490eff9
Huahai Yang <huahai.yang@gmail.com>
2015-02-26 14:26:18 -0800

    don't get list-id from env

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # clj-sendy
 
-A Clojure client for Sendy API
+[Sendy](http://sendy.co/) is an email newsletter/marketing application that uses Amazon SES to send emails.  This Clojure client for Sendy provides a few functions to work with Sendy. 
 
-[Sendy](http://sendy.co/) is an email newsletter/marketing application that uses Amazon SES to send emails. 
+Configurations are expected to be in the following enviornment variables:
 
-Use this to work with Sendy in Clojure. Configurations are expected to be in the enviornment variables:
-
-`SENDY_URL` is the base URL of the sendy installation, without trailing "/"
-`LIST_ID` is the ID of the mailing list. 
-`LIST_IDS` is a comma separated IDs of mailing lists to be targeted in a compaign.
-`BRAND_ID` is the ID of the brand.
-`API_KEY` is your API key.
+- `SENDY_URL` is the base URL of the sendy installation, without the trailing "/"
+- `API_KEY` is the Sendy API key.
--- src/clj_sendy/core.clj
+++ src/clj_sendy/core.clj
@@ -3,45 +3,46 @@
             [clj-http.client    :as client]))
 
 (defn subscribe
-  [email & {:keys [name]}]
+  [email list-id & {:keys [name]}]
   (client/post 
-    (str (env :sendy-url) "/subscribe")
-    {:form-params 
-     (let [params {:email email :list (env :list-id) :boolean true}] 
-       (if name (merge params {:name name}) params))}))
+   (str (env :sendy-url) "/subscribe")
+   {:form-params 
+    (let [params {:email email :list list-id :boolean true}] 
+      (if name (merge params {:name name}) params))}))
 
 (defn unsubscribe
-  [email]
+  [email list-id]
   (client/post 
-    (str (env :sendy-url) "/unsubscribe")
-    {:form-params 
-     {:email email :list (env :list-id) :boolean true}}))
+   (str (env :sendy-url) "/unsubscribe")
+   {:form-params 
+    {:email email :list list-id :boolean true}}))
 
 (defn status
-  [email]
+  [email list-id]
   (client/post 
-    (str (env :sendy-url) "/api/subscribers/subscription-status.php")
-    {:form-params 
-     {:email email :list_id (env :list-id) :api_key (env :api-key)}}))
+   (str (env :sendy-url) "/api/subscribers/subscription-status.php")
+   {:form-params 
+    {:email email :list_id list-id :api_key (env :api-key)}}))
 
 (defn subscriber-count
-  []
+  [list-id]
   (client/post 
-    (str (env :sendy-url) "/api/subscribers/active-subscriber-count.php")
-    {:form-params 
-     {:list_id (env :list-id) :api_key (env :api-key)}}))
+   (str (env :sendy-url) "/api/subscribers/active-subscriber-count.php")
+   {:form-params 
+    {:list_id list-id :api_key (env :api-key)}}))
 
 (defn campaign
+  "list-ids is a comma separated string. If send? is false, only campaign draft
+  will be created"
   [from-name from-email reply-to subject
-   plain-text html-text send?]
+   plain-text html-text list-ids brand-id send?]
   (client/post 
-    (str (env :sendy-url) "/api/campaigns/create.php")
-    {:form-params 
-     {:api_key (env :api-key) 
-      :from_name from-name :from_email from-email
-      :reply_to reply-to :subject subject :plain_text plain-text
-      :html_text html-text 
-      :list_ids (env :list-ids) 
-      :brand_id (env :brand-id) 
-      :send_campaign (if send? 1 0)}}))
-
+   (str (env :sendy-url) "/api/campaigns/create.php")
+   {:form-params 
+    {:api_key (env :api-key) 
+     :from_name from-name :from_email from-email
+     :reply_to reply-to :subject subject :plain_text plain-text
+     :html_text html-text 
+     :list_ids list-ids 
+     :brand_id brand-id 
+     :send_campaign (if send? 1 0)}}))
